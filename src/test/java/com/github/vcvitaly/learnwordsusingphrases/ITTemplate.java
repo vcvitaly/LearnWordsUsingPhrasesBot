e6b4d8bcb1d03c49bc50e15eb69083f8a19ba444
Vitalii Chura <vcvitaly@yahoo.com>
2023-02-15 16:11:30 +0200

    update the test to use ITTemplate

--- src/test/java/com/github/vcvitaly/learnwordsusingphrases/ITTemplate.java
+++ src/test/java/com/github/vcvitaly/learnwordsusingphrases/ITTemplate.java
@@ -1,9 +1,23 @@
 package com.github.vcvitaly.learnwordsusingphrases;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.telegram.telegrambots.starter.TelegramBotInitializer;
+
 /**
  * ITTemplate.
  *
  * @author Vitalii Chura
  */
+@ActiveProfiles({"test"})
+@SpringBootTest
 public abstract class ITTemplate {
+
+    @MockBean
+    private TelegramBotInitializer telegramBotInitializer;
+
+    @MockBean
+    private MeterRegistry meterRegistry;
 }
