b5075af71f62df899f8f281a9c11d073fb754975
Vitalii Chura <vcvitaly@yahoo.com>
2023-03-29 09:08:40 +0300

    add subscribe/unsubscribe button mocks

--- src/main/java/com/github/vcvitaly/learnwordsusingphrases/enumeration/Command.java
+++ src/main/java/com/github/vcvitaly/learnwordsusingphrases/enumeration/Command.java
@@ -11,10 +11,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum Command {
-    START("/start", "command_text/start.txt"),
-    HELP("/help", "command_text/help.txt"),
-    SUBSCRIBED_WORDS("/subscribed_words", null);
+    START("start", "command_text/start.txt"),
+    HELP("help", "command_text/help.txt"),
+    SUBSCRIBED_WORDS("subscribed_words", null),
+    SUBSCRIBE("subscribe", null),
+    UNSUBSCRIBE("unsubscribe", null);
 
-    private final String command;
+    private final String data;
     private final String descriptionFilePath;
+
+    public String getCommand() {
+        return "/" + data;
+    }
 }
--- src/main/java/com/github/vcvitaly/learnwordsusingphrases/util/Constants.java
+++ src/main/java/com/github/vcvitaly/learnwordsusingphrases/util/Constants.java
@@ -1,14 +1,16 @@
 package com.github.vcvitaly.learnwordsusingphrases.util;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * Constants.
  *
  * @author Vitalii Chura
  */
-public final class Constants {
-
-    private Constants() {
-    }
+@UtilityClass
+public class Constants {
 
     public static final String NEW_LINE = "\n";
+    public static final String SUBSCRIBE_TEXT = "Subscribe";
+    public static final String UNSUBSCRIBE_TEXT = "Unsubscribe";
 }
