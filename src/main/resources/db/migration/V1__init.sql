192eef69feef83eb1a63b5bfa8875f87f2fb6144
Vitalii Chura <vcvitaly@yahoo.com>
2023-06-28 20:58:27 +0300

    [8] - initialize the database

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,53 @@
+-- users table
+CREATE SEQUENCE public.users_pkey_seq;
+
+ALTER SEQUENCE public.users_pkey_seq
+    OWNER TO postgres;
+
+CREATE TABLE public.users
+(
+    id         bigint NOT NULL DEFAULT nextval('users_pkey_seq'),
+    chat_id    bigint NOT NULL,
+    timezone   integer,
+    created_at date   NOT NULL,
+    updated_at date,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
+
+-- words table
+CREATE SEQUENCE public.words_pkey_seq;
+
+ALTER SEQUENCE public.words_pkey_seq
+    OWNER TO postgres;
+
+CREATE TABLE public.words
+(
+    id          bigint                NOT NULL DEFAULT nextval('words_pkey_seq'),
+    word        character varying(30) NOT NULL,
+    created_at  date                  NOT NULL,
+    img_s3_path varchar(255),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.words
+    OWNER to postgres;
+
+-- saved_words table
+CREATE SEQUENCE public.saved_words_pkey_seq;
+
+ALTER SEQUENCE public.saved_words_pkey_seq
+    OWNER TO postgres;
+
+CREATE TABLE public.saved_words
+(
+    id      bigint NOT NULL DEFAULT nextval('saved_words_pkey_seq'),
+    user_id bigint NOT NULL constraint saved_words_to_users_fk references users,
+    word_id bigint NOT NULL constraint saved_words_to_words_fk references words,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.subscriptions
+    OWNER to postgres;
